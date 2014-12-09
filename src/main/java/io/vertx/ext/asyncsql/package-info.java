96055d4bb7a2099d89cec7ef98d856ee08ce9670
Joern Bernhardt <jb@campudus.com>
2014-12-09 16:39:00 +0100

    Add package-info
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

---
+++ src/main/java/io/vertx/ext/asyncsql/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @author <a href="http://www.campudus.com">Joern Bernhardt</a>.
+ */
+@GenModule(name = "ext-mysql-postgresql")
+package io.vertx.ext.asyncsql;
+
+import io.vertx.codegen.annotations.GenModule;
