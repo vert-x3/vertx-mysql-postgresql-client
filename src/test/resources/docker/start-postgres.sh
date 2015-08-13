d28d8d36fba474ccffc4f3591326f2976e1d3eda
Clement Escoffier <clement.escoffier@gmail.com>
2015-08-13 12:23:38 +0200

    Fix the postgres start script

---
+++ src/test/resources/docker/start-postgres.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+export POSTGRES_DB=testdb
+export POSTGRES_USER=vertx
+export POSTGRES_PASSWORD=password
+
+docker run -d \
+		-e POSTGRES_USER \
+		-e POSTGRES_PASSWORD \
+		-e POSTGRES_DB \
+		--name "some-postgres" \
+		-p 5432:5432 \
+		"postgres"
+
+#		-v "./postgres-initdb.sql:/docker-entrypoint-initdb.d/test.sql":ro \
+
+
+
