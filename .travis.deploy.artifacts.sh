8ff02f463733a1a1145ac5c4bad414a279513a55
Thomas Segismont <tsegismo@redhat.com>
2016-11-21 11:10:59 +0100

    Travis: apply latest config changes
    - Externalize deployment commands
    - Run all steps in script phase because a failure in after_success does
      not fail the build

---
+++ .travis.deploy.artifacts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PROJECT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version -B | grep -v '\[')
+
+if [[ "${PROJECT_VERSION}" =~ .*SNAPSHOT ]] && [[ "${TRAVIS_BRANCH}" = "master" ]] && [[ "${TRAVIS_PULL_REQUEST}" = "false" ]];
+then
+  mvn clean deploy -DskipTests -B;
+fi
