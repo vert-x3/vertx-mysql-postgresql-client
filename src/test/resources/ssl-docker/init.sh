b2ed3dfa45c7f71eef9c31e51dc3b9592dd9cc8a
Joern Bernhardt <jb@campudus.com>
2017-06-26 13:08:04 +0200

    Add test for SSL configuration
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

---
+++ src/test/resources/ssl-docker/init.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+echo "HELLO FROM INIT SCRIPT! $PGDATA test!!!"
+sed -i 's/^host/hostssl/g' "$PGDATA"/pg_hba.conf
+
+cp /docker-entrypoint-initdb.d/server.{crt,key} "$PGDATA"
+chown postgres:postgres "$PGDATA"/server.{crt,key}
+chmod 0600 "$PGDATA"/server.key
+
+echo "pg_hba.conf now:"
+cat "$PGDATA"/pg_hba.conf
+
+sed -ri "s/^#?(ssl\s*=\s*)\S+/\1'on'/" "$PGDATA/postgresql.conf"
+
+cat "$PGDATA"/postgresql.conf
+
+echo "INIT SCRIPT DONE!"
