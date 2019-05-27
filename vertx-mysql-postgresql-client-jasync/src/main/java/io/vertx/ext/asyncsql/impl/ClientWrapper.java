f85cf15bc7e647d0b86c756c09415d03f9984348
严尚君 <ilayan19900722@gmail.com>
2019-05-28 03:37:55 +0800

    Backported `Catch the exception thrown by the synchronization` to 3.6 (#152)
    * catch the exception thrown by the synchronization
    
    * add a test

--- vertx-mysql-postgresql-client-jasync/src/main/java/io/vertx/ext/asyncsql/impl/ClientWrapper.java
+++ vertx-mysql-postgresql-client-jasync/src/main/java/io/vertx/ext/asyncsql/impl/ClientWrapper.java
@@ -18,9 +18,9 @@ package io.vertx.ext.asyncsql.impl;
 
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
+import io.vertx.core.json.JsonArray;
 import io.vertx.ext.asyncsql.AsyncSQLClient;
-import io.vertx.ext.sql.SQLClient;
-import io.vertx.ext.sql.SQLConnection;
+import io.vertx.ext.sql.*;
 
 /**
  * Wraps a client with the {@link ClientHolder} in order to keep track of the references.
@@ -50,5 +50,53 @@ public class ClientWrapper implements AsyncSQLClient {
   @Override
   public SQLClient getConnection(Handler<AsyncResult<SQLConnection>> handler) {
     return client.getConnection(handler);
+  }
+
+  @Override
+  public SQLClient query(String sql, Handler<AsyncResult<ResultSet>> handler) {
+    client.query(sql, handler);
+    return this;
+  }
+
+  @Override
+  public SQLClient queryStream(String sql, Handler<AsyncResult<SQLRowStream>> handler) {
+    client.queryStream(sql, handler);
+    return this;
+  }
+
+  @Override
+  public SQLClient queryStreamWithParams(String sql, JsonArray params, Handler<AsyncResult<SQLRowStream>> handler) {
+    client.queryStreamWithParams(sql, params, handler);
+    return this;
+  }
+
+  @Override
+  public SQLClient queryWithParams(String sql, JsonArray params, Handler<AsyncResult<ResultSet>> handler) {
+    client.queryWithParams(sql, params, handler);
+    return this;
+  }
+
+  @Override
+  public SQLClient update(String sql, Handler<AsyncResult<UpdateResult>> handler) {
+    client.update(sql, handler);
+    return this;
+  }
+
+  @Override
+  public SQLClient updateWithParams(String sql, JsonArray params, Handler<AsyncResult<UpdateResult>> handler) {
+    client.updateWithParams(sql, params, handler);
+    return this;
+  }
+
+  @Override
+  public SQLClient call(String sql, Handler<AsyncResult<ResultSet>> handler) {
+    client.call(sql, handler);
+    return this;
+  }
+
+  @Override
+  public SQLClient callWithParams(String sql, JsonArray params, JsonArray outputs, Handler<AsyncResult<ResultSet>> handler) {
+    client.callWithParams(sql, params, outputs, handler);
+    return this;
   }
 }
