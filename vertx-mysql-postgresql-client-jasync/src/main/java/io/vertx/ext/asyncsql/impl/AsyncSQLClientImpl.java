56fd0cdfe191cca229a87d28155b8205416ff7c2
严尚君 <ilayan19900722@gmail.com>
2019-06-06 17:19:56 +0800

    Revert "Backported `Catch the exception thrown by the synchronization` to 3.6 (#152)"
    This reverts commit f85cf15bc7e647d0b86c756c09415d03f9984348.

--- vertx-mysql-postgresql-client-jasync/src/main/java/io/vertx/ext/asyncsql/impl/AsyncSQLClientImpl.java
+++ vertx-mysql-postgresql-client-jasync/src/main/java/io/vertx/ext/asyncsql/impl/AsyncSQLClientImpl.java
@@ -17,15 +17,12 @@
 package io.vertx.ext.asyncsql.impl;
 
 import io.vertx.core.AsyncResult;
-import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
-import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.asyncsql.AsyncSQLClient;
-import io.vertx.ext.sql.*;
-
-import java.util.function.Function;
+import io.vertx.ext.sql.SQLClient;
+import io.vertx.ext.sql.SQLConnection;
 
 /**
  * @author <a href="http://www.campudus.com">Joern Bernhardt</a>.
@@ -58,68 +55,4 @@ public class AsyncSQLClientImpl implements AsyncSQLClient {
     return this;
   }
 
-  @Override
-  public SQLClient query(String sql, Handler<AsyncResult<ResultSet>> handler) {
-    executeDirect(conn -> Future.future(f -> conn.query(sql, f)), handler);
-    return this;
-  }
-
-  @Override
-  public SQLClient queryStream(String sql, Handler<AsyncResult<SQLRowStream>> handler) {
-    executeDirect(conn -> Future.future(f -> conn.queryStream(sql, f)), handler);
-    return this;
-  }
-
-  @Override
-  public SQLClient queryStreamWithParams(String sql, JsonArray params, Handler<AsyncResult<SQLRowStream>> handler) {
-    executeDirect(conn -> Future.future(f -> conn.queryStreamWithParams(sql, params, f)), handler);
-    return this;
-  }
-
-  @Override
-  public SQLClient queryWithParams(String sql, JsonArray params, Handler<AsyncResult<ResultSet>> handler) {
-    executeDirect(conn -> Future.future(f -> conn.queryWithParams(sql, params, f)), handler);
-    return this;
-  }
-
-  @Override
-  public SQLClient update(String sql, Handler<AsyncResult<UpdateResult>> handler) {
-    executeDirect(conn -> Future.future(f -> conn.update(sql, f)), handler);
-    return this;
-  }
-
-  @Override
-  public SQLClient updateWithParams(String sql, JsonArray params, Handler<AsyncResult<UpdateResult>> handler) {
-    executeDirect(conn -> Future.future(f -> conn.updateWithParams(sql, params, f)), handler);
-    return this;
-  }
-
-  @Override
-  public SQLClient call(String sql, Handler<AsyncResult<ResultSet>> handler) {
-    executeDirect(conn -> Future.future(f -> conn.call(sql, f)), handler);
-    return this;
-  }
-
-  @Override
-  public SQLClient callWithParams(String sql, JsonArray params, JsonArray outputs, Handler<AsyncResult<ResultSet>> handler) {
-    executeDirect(conn -> Future.future(f -> conn.callWithParams(sql, params, outputs, f)), handler);
-    return this;
-  }
-
-  private <T> void executeDirect(Function<SQLConnection, Future<T>> action, Handler<AsyncResult<T>> handler) {
-    getConnection(getConnection -> {
-      if (getConnection.failed()) {
-        handler.handle(Future.failedFuture(getConnection.cause()));
-      } else {
-        final SQLConnection conn = getConnection.result();
-        Future<T> future;
-        try {
-          future = action.apply(conn);
-        } catch (Throwable e) {
-          future = Future.failedFuture(e);
-        }
-        future.setHandler(handler);
-      }
-    });
-  }
 }
